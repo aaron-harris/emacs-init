0d7c76963240b0b41465db4fa523d7bfd9e5021b
Aaron Harris <aaron.patrick.harris@gmail.com>
2015-08-06 17:25:24 -0500

    Added commands for per-line scrolling.

--- aph-functions.el
+++ aph-functions.el
@@ -70,6 +70,24 @@ uncleared outputs)."
   (interactive "p")
   (kill-new (aph/ielm-get-last-output arg)))
 
+(defun aph/scroll-down-by-line (&optional arg)
+  "As `scroll-down-command', but ARG defaults to 1.
+
+Also, a negative prefix argument is treated as -1, scrolling only
+one line upward."
+  ;; All of our changes are encapsulated in the `interactive' form.
+  (interactive "^p")
+  (scroll-down-command arg))
+
+(defun aph/scroll-up-by-line (&optional arg)
+  "As `scroll-up-command', but ARG defaults to 1.
+
+Also, a negative prefix argument is treated as -1, scrolling only
+one line downward."
+  ;; All of our changes are encapsulated in the `interactive' form.
+  (interactive "^p")
+  (scroll-up-command arg))
+
 
 ;;; Utility Functions
 ;;;==================
