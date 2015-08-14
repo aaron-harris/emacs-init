c4d963f29e82d7b583bfd5dbcd19261267b3f160
Aaron Harris <meerwolf@gmail.com>
2015-08-14 13:24:42 -0500

    Cleaned up implementation of aph-define-keys.

--- aph-keys.el
+++ aph-keys.el
@@ -12,27 +12,19 @@
 ;; Functions in this section make defining keys safer, handling
 ;; exceptional conditions more gracefully than the default functions.
 
-(defun aph/define-key-safely (keymap key command
-                                     &optional package nomsg-on-rebind)
+(defun aph/define-key-safely (keymap key command &optional nomsg-on-rebind)
   "As `define-key', but check if COMMAND is defined first.
 
 If COMMAND is defined, proceed to bind it to KEY in KEYMAP.  If
-it isn't, print a message to that effect and do not bind it.
+it isn't, print a message to that effect and do not bind it. 
 
-If the first optional parameter PACKAGE is supplied, delay the
-keybinding until after PACKAGE is loaded, using
-`eval-after-load'.
-
-Unless the second optional parameter NOMSG-ON-REBIND is supplied,
-also print a message if we are overwriting an existing binding
-for KEY.  Proceed with the rebinding in any case.
+Unless the optional parameter NOMSG-ON-REBIND is supplied, also
+print a message if we are overwriting an existing binding for
+KEY.  Proceed with the rebinding in any case.
 
 If the binding succeeds, return COMMAND.  Otherwise return nil."
-  (if package
-      (eval-after-load package
-        '(aph/define-key-safely keymap key command nil nomsg-on-rebind))
-    (catch 'esc
-      (let ((old-binding (lookup-key keymap key)))
+  (catch 'esc
+    (let ((old-binding (lookup-key keymap key)))
       (if (fboundp command)
           (define-key keymap key command)
         (message "Cannot bind #'%s to %s: Command not defined"
@@ -43,39 +35,32 @@ If the binding succeeds, return COMMAND.  Otherwise return nil."
                   (eq old-binding command))
         (message "Warning: Binding #'%s to %s overwrites existing binding #'%s"
                  command (key-description key) old-binding))
-      command))))
+      command)))
 
-(defmacro aph/define-keys-safely (keymap package &rest bindings)
+(defmacro aph/define-keys-safely (keymap &rest bindings)
   "Define multiple keys using `aph/define-key-safely'.
 
 All BINDINGS must be for the same KEYMAP, and the PACKAGE
 parameter applies to all keybindings.  Each binding should be of
 the form (KEY COMMAND NOMSG-ON-REBIND).  See
-`aph/define-key-safely' for details.
-
-Unlike `aph/define-key-safely', the parameter PACKAGE is not
-optional and must be explicitly set to nil if its effect is not
-needed."
-  (declare (indent 2))
+`aph/define-key-safely' for details." 
+  (declare (indent 1))
   `(progn
      ,@(mapcar
         (lambda (keydef)
           (let ((key              (nth 0 keydef))
                 (command          (nth 1 keydef))
                 (nomsg-on-rebind  (nth 2 keydef)))
-            `(aph/define-key-safely ,keymap ,key ,command
-                                    ,package ,nomsg-on-rebind)))
+            `(aph/define-key-safely ,keymap ,key ,command ,nomsg-on-rebind)))
         bindings)))
 
-(defun aph/global-set-key-safely (key command
-                                      &optional package nomsg-on-rebind)
+(defun aph/global-set-key-safely (key command &optional nomsg-on-rebind)
   "As `aph/define-key-safely', but use the global keymap."
-  (aph/define-key-safely (current-global-map)
-                         key command package nomsg-on-rebind))
+  (aph/define-key-safely (current-global-map) key command nomsg-on-rebind))
 
-(defmacro aph/global-set-keys-safely (package &rest bindings)
+(defmacro aph/global-set-keys-safely (&rest bindings)
   "As `aph/define-keys-safely', but use the global keymap."
-  (declare (indent 1)) 
-  `(aph/define-keys-safely (current-global-map) ,package ,@bindings))
+  (declare (indent 0))
+  `(aph/define-keys-safely (current-global-map) ,@bindings))
 
 (provide 'aph-keys)
--- init-keys.el
+++ init-keys.el
@@ -20,7 +20,7 @@
 ;; Removing Unnecessary Bindings
 (global-unset-key (kbd "C-z"))          ; I don't need minimization on a key.
 
-(aph/global-set-keys-safely (not :pkg)
+(aph/global-set-keys-safely
   ;; Scrolling Commands
   ((kbd "<down>")    #'aph/scroll-up-by-line      :rebind)
   ((kbd "<up>")      #'aph/scroll-down-by-line    :rebind) 
@@ -41,95 +41,91 @@
 
 ;;; Smartparens Keybindings
 ;;;========================
-(aph/define-keys-safely smartparens-mode-map 'smartparens 
-  ;; Movement Commands
-  ((kbd "C-M-b")           #'sp-backward-sexp)
-  ((kbd "C-M-f")           #'sp-forward-sexp)
-  ((kbd "C-M-u")           #'sp-backward-up-sexp)
-  ((kbd "C-M-d")           #'sp-down-sexp)
-  ((kbd "M-B")             #'sp-backward-symbol)
-  ((kbd "M-F")             #'sp-forward-symbol)
-  ;; Selection Commands
-  ((kbd "C-]")             #'sp-select-next-thing-exchange)
-  ((kbd "C-M-]")           #'sp-select-previous-thing)
-  ;; Barf and Slurp Commands
-  ((kbd "C-<left>")        #'sp-forward-barf-sexp)
-  ((kbd "C-<right>")       #'sp-forward-slurp-sexp)
-  ((kbd "C-S-<left>")      #'sp-backward-slurp-sexp)
-  ((kbd "C-S-<right>")     #'sp-backward-barf-sexp)
-  ;; Kill and Copy Commands
-  ((kbd "C-M-k")           #'sp-kill-sexp)
-  ((kbd "C-M-w")           #'sp-copy-sexp)
-  ((kbd "C-S-<backspace>") #'sp-splice-sexp-killing-around)
-  ;; Unwrap and Splice Commands
-  ((kbd "C-<delete>")      #'sp-unwrap-sexp)
-  ((kbd "C-<backspace>")   #'sp-backward-unwrap-sexp)
-  ((kbd "M-D")             #'sp-splice-sexp)
-  ;; Reconfiguration Commands
-  ((kbd "C-M-t")           #'sp-transpose-sexp)
-  ((kbd "M-)")             #'sp-up-sexp)
-  ;; Other Commands
-  ((kbd "C-x n (")         #'sp-narrow-to-sexp))
-
-(aph/define-keys-safely smartparens-strict-mode-map 'smartparens
-  ((kbd ")")   #'sp-up-sexp))
+(eval-after-load 'smartparens
+  '(progn
+     (aph/define-keys-safely smartparens-mode-map
+       ;; Movement Commands
+       ((kbd "C-M-b")           #'sp-backward-sexp)
+       ((kbd "C-M-f")           #'sp-forward-sexp)
+       ((kbd "C-M-u")           #'sp-backward-up-sexp)
+       ((kbd "C-M-d")           #'sp-down-sexp)
+       ((kbd "M-B")             #'sp-backward-symbol)
+       ((kbd "M-F")             #'sp-forward-symbol)
+       ;; Selection Commands
+       ((kbd "C-]")             #'sp-select-next-thing-exchange)
+       ((kbd "C-M-]")           #'sp-select-previous-thing)
+       ;; Barf and Slurp Commands
+       ((kbd "C-<left>")        #'sp-forward-barf-sexp)
+       ((kbd "C-<right>")       #'sp-forward-slurp-sexp)
+       ((kbd "C-S-<left>")      #'sp-backward-slurp-sexp)
+       ((kbd "C-S-<right>")     #'sp-backward-barf-sexp)
+       ;; Kill and Copy Commands
+       ((kbd "C-M-k")           #'sp-kill-sexp)
+       ((kbd "C-M-w")           #'sp-copy-sexp)
+       ((kbd "C-S-<backspace>") #'sp-splice-sexp-killing-around)
+       ;; Unwrap and Splice Commands
+       ((kbd "C-<delete>")      #'sp-unwrap-sexp)
+       ((kbd "C-<backspace>")   #'sp-backward-unwrap-sexp)
+       ((kbd "M-D")             #'sp-splice-sexp)
+       ;; Reconfiguration Commands
+       ((kbd "C-M-t")           #'sp-transpose-sexp)
+       ((kbd "M-)")             #'sp-up-sexp)
+       ;; Other Commands
+       ((kbd "C-x n (")         #'sp-narrow-to-sexp)) 
+     (aph/define-keys-safely smartparens-strict-mode-map
+       ((kbd ")")               #'sp-up-sexp))))
 
 
 ;;; Org Mode Keybindings
 ;;;=====================
-(aph/global-set-keys-safely 'org
+(aph/global-set-keys-safely
   ;; Agenda Commands
   ((kbd "C-c a")   #'org-agenda)
   ((kbd "<f1>")    #'aph/org-agenda-display-smart-agenda :rebind)
   ;; Capture and Refile Commands
-  ((kbd "C-c c")   #'org-capture)
-  ((kbd "C-c w")   #'aph/org-goto-last-refile)
+  ((kbd "C-c c")   #'org-capture) 
   ;; Link Commands
-  ((kbd "C-c l")   #'org-store-link)
-  ;; Clocking Commands 
-  ((kbd "C-c t j") #'org-clock-goto)
-  ((kbd "C-c t o") #'org-clock-out)
-  ((kbd "C-c t x") #'org-clock-cancel)
-  ((kbd "C-c t r") #'org-clock-in-last))
-
-(aph/define-keys-safely org-mode-map 'org
-  ;; Clocking Commands
-  ((kbd "C-c t i") #'org-clock-in) 
-  ;; Spinner Commands
-  ((kbd "C-z s")   #'aph/org-spin-basic)
-  ((kbd "C-z C-s") #'aph/org-spin-weighted))
+  ((kbd "C-c l")   #'org-store-link))
+(eval-after-load 'org
+  '(progn
+     (aph/global-set-keys-safely
+       ;; Capture and Refile Commands 
+       ((kbd "C-c w")   #'aph/org-goto-last-refile) 
+       ;; Clocking Commands 
+       ((kbd "C-c t j") #'org-clock-goto)
+       ((kbd "C-c t o") #'org-clock-out)
+       ((kbd "C-c t x") #'org-clock-cancel)
+       ((kbd "C-c t r") #'org-clock-in-last))
+     (aph/define-keys-safely org-mode-map
+       ;; Clocking Commands
+       ((kbd "C-c t i") #'org-clock-in) 
+       ;; Spinner Commands
+       ((kbd "C-z s")   #'aph/org-spin-basic)
+       ((kbd "C-z C-s") #'aph/org-spin-weighted))))
 
 
 ;;; Programming Keybindings
 ;;;========================
 ;; Elisp
-(aph/global-set-keys-safely (not :pkg)
+(aph/global-set-keys-safely
   ((kbd "C-x M-l") #'find-library)
   ((kbd "C-:")     #'pp-eval-expression))
 
-;; This next form errors if the `eval-after-load' is removed,
-;; precisely because `ielm-map' is not defined until after 'ielm is
-;; loaded.  I may need to rethink my implementation of
-;; `aph/define-keys-safely'.
 (eval-after-load 'ielm
-  '(aph/define-keys-safely ielm-map 'ielm
-    ((kbd "C-c M-w") #'aph/ielm-copy-last-output)
-    ((kbd "C-c C-t") #'aph/eval-expression-toggle-clean-output)))
+  '(aph/define-keys-safely ielm-map
+     ((kbd "C-c M-w") #'aph/ielm-copy-last-output)
+     ((kbd "C-c C-t") #'aph/eval-expression-toggle-clean-output)))
 
 ;; Clojure and Cider
-(aph/global-set-keys-safely (not :pkg)
-  ((kbd "C-c M-c") #'cider-connect))
+(aph/global-set-key-safely (kbd "C-c M-c") #'cider-connect)
 
 
 ;;; Other Keybindings
 ;;;==================
 ;; Eww
 
-;; Again, we can't strip the `eval-after-load' until we fix
-;; `aph/define-keys-safely'.
 (eval-after-load 'eww
-  '(aph/define-keys-safely eww-mode-map 'eww
-    ((kbd "S-<tab") #'shr-previous-link)))
+  '(aph/define-key-safely eww-mode-map (kbd "S-<tab") #'shr-previous-link))
 
 
 ;;; Machine-Specific Keybindings
