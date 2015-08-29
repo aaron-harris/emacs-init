ae8505647d2b93d283edfa179aa12b5d84b6328f
Aaron Harris <aaron.patrick.harris@gmail.com>
2015-08-29 16:45:53 -0500

    Begin drafting code to make Org-mode capture in new frame

--- init-draft.el
+++ init-draft.el
@@ -19,3 +19,36 @@
 
 (define-key read-expression-map [(shift tab)] 'hippie-unexpand)
 
+
+;;; Make capture occur in new frame
+;;;==================================
+(defvar aph/org-capture-in-popout-frame t
+  "If non-nil, `org-capture' does its work in a new frame.")
+
+;; What we need to do:
+;; - Make `org-capture-select-template' use a pop-up frame,
+;;   and then keep it around.
+;; --> `org-capture-select-template' delegates to
+;;     `org-switch-to-buffer-other-window', so that's what we need to
+;;     advise.
+;; - Probably need to fix the value stored as :return-to-wconf.
+;; - Make `org-capture-fill-template' reuse the same capture frame.
+
+(defun aph/org-switch-to-buffer-in-capture-frame (&rest args)
+  "Advice to support `aph/org-capture-in-popout-frame'.
+When this variable is non-nil, override the usual behavior of
+`org-switch-to-buffer-other-window' and open the buffer described
+by ARGS in a new frame.
+
+Otherwise, return nil to return control to
+`org-switch-to-buffer-other-window'."
+  (when aph/org-capture-in-popout-frame
+    (apply #'switch-to-buffer-other-frame args)))
+
+(advice-add #'org-switch-to-buffer-other-window
+            :before-until
+            #'aph/org-switch-to-buffer-in-capture-frame)
+
+;; To clean up WIP code:
+(advice-remove #'org-switch-to-buffer-other-window 
+               #'aph/org-switch-to-buffer-in-capture-frame)
