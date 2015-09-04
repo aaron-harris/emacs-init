c8b0891b07b8b0a595d4976b4c5bb0cae305f86e
Aaron Harris <meerwolf@gmail.com>
2015-09-04 16:29:25 -0500

    Continue drafting "capture in new frame" code for Org-mode
    This continues work on the feature started in commit affd46aeb798 that
    seeks to make all of the buffers used in the Org-mode capture process
    do their work in a separate frame.  Work is still incomplete.

--- init-draft.el
+++ init-draft.el
@@ -38,11 +38,13 @@
   "Advice to support `aph/org-capture-in-popout-frame'.
 When this variable is non-nil, override the usual behavior of
 `org-switch-to-buffer-other-window' and open the buffer described
-by ARGS in a new frame.
+by ARGS in a frame named \"Capture\", creating such a frame if
+necessary.
 
 Otherwise, return nil to return control to
 `org-switch-to-buffer-other-window'."
-  (when aph/org-capture-in-popout-frame
+  (when aph/org-capture-in-popout-frame 
+    
     (apply #'switch-to-buffer-other-frame args)))
 
 (advice-add #'org-switch-to-buffer-other-window
@@ -52,3 +54,52 @@ Otherwise, return nil to return control to
 ;; To clean up WIP code:
 (advice-remove #'org-switch-to-buffer-other-window 
                #'aph/org-switch-to-buffer-in-capture-frame)
+
+;; A new approach: This stackexhange thread discusses use of
+;; `display-buffer-alist':
+;; http://emacs.stackexchange.com/questions/2194/how-do-i-force-a-specific-buffer-to-open-in-a-new-window
+;; This may be a more promising approach.  See also documentation for
+;; `display-buffer'.
+
+;; The first thing to do is to find or write a function that displays
+;; a buffer in a named frame, creating the frame if it does not exist.
+(defun aph/get-frame-by-name (fname)
+  "If there is a frame with NAME, return it, else nil."
+  (-some (lambda (frame)
+           (when (equal fname (frame-parameter frame 'name))
+             frame))
+         (frame-list)))
+
+(defun aph/display-buffer-in-named-frame (buffer alist)
+  "Display BUFFER in frame with specific name.
+The name to use is the value associated with the 'named-frame key
+in ALIST.  If a frame with that name already exists, use it.
+Otherwise, call `display-buffer-in-pop-up-frame' to create it.
+
+If ALIST does not contain the key 'named-frame, use the name of BUFFER."
+  (let* ((fname  (or (cdr (assq 'named-frame alist))
+                     (buffer-name buffer)))
+         (frame  (aph/get-frame-by-name fname)))
+    (if frame
+        (window--display-buffer buffer
+                                (frame-selected-window frame)
+                                'reuse)
+      ;; TODO: Modify alist to make sure it includes the name.
+      (display-buffer-pop-up-frame
+       buffer
+       (add-to-list 'alist `(pop-up-frame-parameters
+                             (name . ,fname)))))))
+
+(setq display-buffer-alist
+      '(("\\*Org Select\\*\\|\\*Capture\\*\\|CAPTURE-.*"
+         aph/display-buffer-in-named-frame
+         (named-frame . "Capture"))))
+
+;; Problems still to be addressed with the new approach.:
+;; - The *Org Select* buffer is still not showing up in the frame.
+;; - When the *Capture* buffer is up, all but one of the windows in
+;;   the original frame have been deleted (though they are restored
+;;   properly at the end of the process)
+;; - The CAPTURE-... buffer is also not showing up in the frame.
+;; - The capture frame sticks around after the capture process is
+;;   complete.
