c59ffde0969dee1695f75e09923c3b31ea17111f
Aaron Harris <meerwolf@gmail.com>
2015-09-10 16:12:50 -0500

    Use `xahk-mode` to edit .ahk files

---
+++ init-ahk.el
@@ -0,0 +1,10 @@
+;;; -*- lexical-binding: t -*-
+
+;;;; The Emacs init file of Aaron Harris.
+;;;; AHK CONFIGURATION
+;;;;============================================================================
+
+;; Use `xahk-mode' to edit .ahk files
+(add-to-list 'auto-mode-alist '("\\.ahk\\'" . xahk-mode))
+
+(provide 'init-ahk)
