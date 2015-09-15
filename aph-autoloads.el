f794ccc121079963133fb057cb18c7dfe29896ea
Aaron Harris <aaron.patrick.harris@gmail.com>
2015-09-15 11:55:26 -0500

    Rename init-autoloads.el to aph-autoloads.el
    This name better fits our naming convention, wherein files with the
    "aph" prefix are regarded as libraries or packages, to be required and
    loaded only as needed (and thus comprising the source of all of our
    autoloads).

--- aph-autoloads.el
+++ aph-autoloads.el
@@ -1,4 +1,4 @@
-;;; init-autoloads.el --- automatically extracted autoloads
+;;; aph-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
 
@@ -95,11 +95,11 @@ Store the current eww url as an Org-Mode link.
 
 ;;;***
 
-(provide 'init-autoloads)
+(provide 'aph-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
 ;; coding: utf-8
 ;; End:
-;;; init-autoloads.el ends here
+;;; aph-autoloads.el ends here
