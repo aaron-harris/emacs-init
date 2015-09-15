59c3b698258c089a45d25d4b2771d4701ad1b04f
Aaron Harris <aaron.patrick.harris@gmail.com>
2015-09-15 11:52:18 -0500

    Break ielm functions out into new file aph-ielm.el

--- aph-functions.el
+++ aph-functions.el
@@ -134,43 +134,6 @@ you want to quit windows on all frames."
       (ignore-errors
         (quit-windows-on buffer kill frame)))))
 
-(defun aph/ielm-get-last-output (&optional arg)
-  "Return the last output produced by `ielm'.
-
-With argument N > 0, instead return the Nth last output.
-With argument N < 0, return the Nth output since last clear.
-With argument N = 0, do nothing and return nil.
-
-If N greater in absolute value than the number of uncleared
-outputs in the ielm buffer, return nil."
-  (let* ((arg              (or arg 1)) 
-         (output-regexp    (concat "^" ielm-prompt ".+\n"
-                                   "\\(?:\s.+\n\\)*"
-                                   "\\([^\s].+\\)"))
-        (search-function  (if (< arg 0)
-                              #'search-forward-regexp
-                            #'search-backward-regexp)))
-    (unless (zerop arg)
-      (with-current-buffer "*ielm*"
-        (save-excursion 
-          (goto-char (if (> arg 0) (point-max) (point-min)))
-          (if (funcall search-function output-regexp nil :noerror (abs arg))
-              (match-string 1)
-            nil))))))
-
-(defun aph/ielm-copy-last-output (&optional arg)
-  "Copy the last output produced by `ielm' to the kill ring.
-
-With argument N > 0, instead copy the Nth last output.
-With argument N < 0, copy the Nth output since last clear.
-With argument N = 0, do nothing.
-
-Return the newly copied string, or nil if nothing was
-copied (e.g., if the argument is greater than the number of
-uncleared outputs)."
-  (interactive "p")
-  (kill-new (aph/ielm-get-last-output arg)))
-
 (defun aph/newline (n)
   "As `newline', with support for negative argument.
 An argument of -N calls `join-line' N times."
