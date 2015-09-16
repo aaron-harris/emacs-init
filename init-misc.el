88e75bded03cef20fdda71353a8d341a868e36fd
Aaron Harris <aaron.patrick.harris@gmail.com>
2015-09-16 16:45:03 -0500

    Fix `rectangle-mark-mode` motion commands
    These commands (bound to `C-b` and `C-f` in `rectangle-mark-mode`)
    were introduced in Emacs 25 and are not currently handling their
    prefix arguments correctly (they enter an infinite loop when given any
    argument other than 1, 0, or -1).  This commit fixes the problem by
    advising the underlying function, `rectangle--*-char`.

--- init-misc.el
+++ init-misc.el
@@ -84,4 +84,29 @@
 (put 'narrow-to-page  'disabled nil)
 (put 'upcase-region   'disabled nil)
 
+
+;;; Rectangle Patch
+;;;================
+;; There seems to be a bug in Emacs 25 where movement commands in
+;; rectangle mode hang when given a prefix argument.  This is an a
+;; workaround until the issue is fixed.
+(defun aph/rectangle-repetition-fix (fn cmd n &optional other-cmd)
+  "Advice to fix bug in `rectangle-mark-mode' motion commands.
+
+The basic cursor motion commands in `rectangle-mark-mode' that
+were introduced in Emacs 25 do not currently handle their prefix
+arguments correctly.  These commands (principally
+`rectangle-forward-char' and `rectangle-backward-char') delegate
+to `rectangle--*-char'.  This function fixes the problem when
+used as advice :around `rectangle--*-char'."
+  (cond
+   ((< n 0)  (aph/rectangle-repetition-fix fn other-cmd (- n)))
+   ((= n 0)  (funcall fn cmd 0 other-cmd))
+   ((> n 0)  (dotimes (i (1- n) (funcall fn cmd 1 other-cmd))
+               (funcall fn cmd 1 other-cmd)))))
+
+;; Since the function `rectangle--*-char' is not defined in Emacs 24,
+;; this next statement will fail silently if we don't need the fix.
+(advice-add #'rectangle--*-char :around #'aph/rectangle-repetition-fix)
+
 (provide 'init-misc)
