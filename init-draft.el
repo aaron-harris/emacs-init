18c6e58d5356a517ce32da943ae7ed3f924bbabf
Aaron Harris <meerwolf@gmail.com>
2015-09-21 17:07:42 -0500

    Begin drafting "sliding door" commands
    These commands will move buffers from one window to another as if they
    were sliding doors.
    
    The first command being drafted is `aph/slide-buffer-forward`, which
    moves the current buffer one window forward in the usual ordering and
    restores the previous buffer displayed in the current window.

--- init-draft.el
+++ init-draft.el
@@ -18,3 +18,19 @@
   (hippie-expand -1))
 
 (define-key read-expression-map [(shift tab)] 'hippie-unexpand)
+
+
+;;; Sliding door commands
+(defun aph/slide-buffer-forward ()
+  "Slide active buffer to next window.
+Display this buffer in the next window forward (in the same
+ordering as `other-window') that is not dedicated to its current
+buffer, and display in this window the previous buffer displayed
+here (using `switch-to-prev-buffer')."
+  (interactive)
+  (let ((buf  (current-buffer))
+        (win  (get-window-with-predicate
+               (lambda (win) (not (window-dedicated-p win))))))
+    (when win
+      (set-window-buffer win buf)
+      (switch-to-prev-buffer))))
