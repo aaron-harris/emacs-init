ae0d862567828efcc985dd55eb657c93efcff60d
Aaron Harris <aaron.patrick.harris@gmail.com>
2015-09-23 22:03:18 -0500

    Finish drafting sliding window commands
    The new commands are:
    - `aph/slide-buffer-forward`, bound to `s-}`
    - `aph/slide-buffer-backward`
    - `aph/swap-buffer-forward`, bound to `s-|`
    - `aph/swap-buffer-backward`
    - `aph/swap-buffer-forward-and-ride`, bound to `s-\`
    - `aph/pull-buffer-backward`, bound to `s-{`
    - `aph/pull-buffer-forward`
    
    To support these commands, there are new utility functions:
    - `aph/get-nth-window-with-predicate`
    - `aph/get-nth-window-not-dedicated`
    - `aph/slide-buffer`
    - `aph/swap-buffers`

--- init-draft.el
+++ init-draft.el
@@ -18,26 +18,3 @@
   (hippie-expand -1))
 
 (define-key read-expression-map [(shift tab)] 'hippie-unexpand)
-
-
-;;; Sliding door commands
-(defun aph/get-nth-window-with-predicate
-    (n predicate &optional minibuf all-frames default)
-  "As `get-window-with-predicate', but return the Nth success."
-  ;; TODO: Write this function.
-  )
-
-(defun aph/slide-buffer-forward (&optional count)
-  "Slide active buffer to another window.
-
-Display this buffer N windows forward (in the same ordering as
-`other-window'), skipping windows dedicated to their current
-buffers, and display in this window the previous buffer displayed
-here (using `switch-to-prev-buffer')."
-  (interactive "p")
-  (let ((buf  (current-buffer))
-        (win  (aph/get-nth-window-with-predicate
-               n (lambda (win) (not (window-dedicated-p win))))))
-    (when win 
-      (set-window-buffer win buf)
-      (switch-to-prev-buffer))))
