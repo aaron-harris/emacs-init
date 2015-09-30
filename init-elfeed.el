9e07ca0ed046e38e9dcd08657dbc0dfbd3ee3b5c
Aaron Harris <aaron.patrick.harris@gmail.com>
2015-09-30 17:30:41 -0500

    Add category tags to Elfeed feeds
    Current tags used are: `comics`, `math`, `programming`,
    `stackoverflow`, `blog`, `history`, `rpg`, `comics`, and `games`.
    
    Tags that are specific to a feed and that might be useful to find a
    particular entry may be placed by hand.

--- init-elfeed.el
+++ init-elfeed.el
@@ -20,82 +20,90 @@
 ;;;==============
 (setq
  elfeed-feeds
- '("http://250bpm.com/feed/pages/pagename/start/category/blog/t/250bpm-blogs/h/"
-   "http://alasdairstuart.com/?feed=rss2"
-   "http://allkindsofhistory.wordpress.com/feed/" 
-   "http://amitp.blogspot.com/feeds/posts/default"
-   "http://blog.8thlight.com/feed/atom.xml"
-   "http://blog.plover.com/index.rss"
-   "http://campcomic.com/rss"
-   "http://cdn.drivecomic.com/rss.xml"
-   "http://cdn.sheldoncomics.com/rss.xml"
-   "http://cthulhuslippers.com/feed/"
-   "http://egscomics.com/rss.php" 
-   "http://emacsredux.com/atom.xml"
-   "http://erfworld.com/erf_stream/view?view=rss"
-   "http://feeds.feedburner.com/NotInventedHere"
-   "http://feeds.feedburner.com/Rik0TechTemple"
-   "http://feeds.feedburner.com/ScenesFromAMultiverse"
-   "http://feeds.feedburner.com/StuartSierra"
-   "http://feeds.feedburner.com/abominable"
-   "http://feeds.feedburner.com/davesquared"
-   "http://feeds.feedburner.com/hijinksensue?format=xml"
+ '(;; Comics
+   ("http://campcomic.com/rss" comics)
+   ("http://cdn.drivecomic.com/rss.xml" comics)
+   ("http://cdn.sheldoncomics.com/rss.xml" comics)
+   ("http://cthulhuslippers.com/feed/" comics)
+   ("http://egscomics.com/rss.php" comics)
+   ("http://emacsredux.com/atom.xml" comics)
+   ("http://erfworld.com/erf_stream/view?view=rss" comics)
+   ("http://feeds.feedburner.com/NotInventedHere" comics)
+   ("http://feeds.feedburner.com/ScenesFromAMultiverse" comics)
+   ("http://feeds.feedburner.com/abominable" comics)
+   ("http://feeds.feedburner.com/hijinksensue?format=xml" comics)
+   ("http://feeds.feedburner.com/rsspect/fJur" comics)
+   ("http://feeds.feedburner.com/uclick/basicinstructions" comics)
+   ("http://feeds.feedburner.com/uclick/foxtrot" comics)
+   ("http://feeds.penny-arcade.com/pa-mainsite/" comics)
+   ("http://flakypastry.runningwithpencils.com/flakypastryrss.xml" comics)
+   ("http://lastmechanicalmonster.blogspot.com/feeds/posts/default" comics)
+   ("http://over-encumbered.com/rss" comics)
+   ("http://plox-comic.com/feed/" comics)
+   ("http://ps238.nodwick.com/?feed=rss2" comics)
+   ("http://pvponline.com/feed" comics)
+   ("http://samandfuzzy.com/rss.php" comics)
+   ("http://skin-horse.com/feed/" comics)
+   ("http://tabletitans.com/feed" comics)
+   ("http://www.alicegrove.com/rss" comics)
+   ("http://www.baldwinpage.com/annagalactic/feed/" comics)
+   ("http://www.bugmartini.com/feed/" comics)
+   ("http://www.cad-comic.com/rss/rss.xml" comics)
+   ("http://www.dumbingofage.com/feed/" comics)
+   ("http://www.giantitp.com/comics/oots.rss" comics)
+   ("http://www.girlgeniusonline.com/ggmain.rss" comics)
+   ("http://www.kiwiblitz.com/rss.php" comics)
+   ("http://www.questionablecontent.net/QCRSS.xml" comics)
+   ("http://www.smbc-comics.com/rss.php" comics)
+   ("http://www.somethingpositive.net/sp.xml" comics)
+   ("http://www.useswordonmonster.com/?feed=rss2" comics)
+   ("http://xkcd.com/atom.xml" comics)
+   ("http://youdamnkid.com/?feed=rss2" comics)
+   ("http://frivolesque.com/feed" comics)
+   ;; Math
+   ("http://blog.plover.com/index.rss" math)
+   ("http://golem.ph.utexas.edu/category/atom10.xml" math)
+   ("http://gottwurfelt.wordpress.com/feed/" math)
+   ("http://gowers.wordpress.com/feed/" math)
+   ("http://igorpak.wordpress.com/feed/" math)
+   ("http://j2kun.svbtle.com/feed" math)
+   ("http://jeremykun.wordpress.com/feed/" math)
+   ("http://quomodocumque.wordpress.com/feed/" math)
+   ("http://rigtriv.wordpress.com/feed/" math)
+   ("http://terrytao.wordpress.com/feed/" math)
+   ("http://www.ams.org/featurecolumn.rss" math)
+   ("http://www.goodmath.org/blog/?feed=rss2" math)
+   ("http://www.scottaaronson.com/blog/?feed=rss2" math)
+   ;; Programming
+   ("http://250bpm.com/feed/pages/pagename/start/category/blog/t/250bpm-blogs/h/"
+    programming)
+   ("http://amitp.blogspot.com/feeds/posts/default" programming)
+   ("http://blog.8thlight.com/feed/atom.xml" programming)
+   ("http://feeds.feedburner.com/Rik0TechTemple" programming)
+   ("http://feeds.feedburner.com/StuartSierra" programming)
+   ("http://feeds.feedburner.com/davesquared" programming)
+   ("http://kotka.de/blog/index.rss" programming)
+   ("http://oremacs.com/atom.xml" programming)
+   ("http://pragmaticemacs.com/feed/" programming)
+   ("http://programming-puzzler.blogspot.com/feeds/posts/default" programming)
+   ("http://simblob.blogspot.com/feeds/posts/default" programming)
+   ("http://swannodette.github.io/atom.xml" programming)
+   ("https://aphyr.com/posts.atom" programming)
+   ("https://yoo2080.wordpress.com/feed/" programming)
+   ;; Stackoverflow
+   ("http://stackoverflow.com/feeds/tag?tagnames=clojure&sort=newest"
+    programming stackoverflow)
+   ("http://stackoverflow.com/feeds/tag?tagnames=emacs&sort=newest"
+    programming stackoverflow)
+   ;; Other
+   ("http://alasdairstuart.com/?feed=rss2" blog)
+   ("http://allkindsofhistory.wordpress.com/feed/" history)
    "http://feeds.feedburner.com/podiobooks?format=xml"
-   "http://feeds.feedburner.com/rsspect/fJur"
-   "http://feeds.feedburner.com/uclick/basicinstructions"
-   "http://feeds.feedburner.com/uclick/foxtrot"
-   "http://feeds.penny-arcade.com/pa-mainsite/" 
-   "http://flakypastry.runningwithpencils.com/flakypastryrss.xml"
-   "http://golem.ph.utexas.edu/category/atom10.xml"
-   "http://gottwurfelt.wordpress.com/feed/"
-   "http://gowers.wordpress.com/feed/"
-   "http://igorpak.wordpress.com/feed/"
-   "http://j2kun.svbtle.com/feed"
-   "http://jeremykun.wordpress.com/feed/"
-   "http://kotka.de/blog/index.rss"
-   "http://lastmechanicalmonster.blogspot.com/feeds/posts/default"
-   "http://mathbabe.org/feed/"
-   "http://oremacs.com/atom.xml"
-   "http://over-encumbered.com/rss"
-   "http://plox-comic.com/feed/" 
-   "http://pragmaticemacs.com/feed/"
-   "http://programming-puzzler.blogspot.com/feeds/posts/default"
-   "http://ps238.nodwick.com/?feed=rss2"
-   "http://pvponline.com/feed"
-   "http://quomodocumque.wordpress.com/feed/"
-   "http://rigtriv.wordpress.com/feed/"
-   "http://ronininstitute.org/feed/"
-   "http://samandfuzzy.com/rss.php"
-   "http://simblob.blogspot.com/feeds/posts/default"
-   "http://skin-horse.com/feed/"
-   "http://stackoverflow.com/feeds/tag?tagnames=clojure&sort=newest"
-   "http://stackoverflow.com/feeds/tag?tagnames=emacs&sort=newest"
-   "http://swannodette.github.io/atom.xml"
-   "http://tabletitans.com/feed"
-   "http://terrytao.wordpress.com/feed/"
-   "http://thealexandrian.net/feed"
-   "http://what-if.xkcd.com/feed.atom"
-   "http://www.alicegrove.com/rss"
-   "http://www.ams.org/featurecolumn.rss" 
-   "http://www.baldwinpage.com/annagalactic/feed/"
-   "http://www.bugmartini.com/feed/"
-   "http://www.cad-comic.com/rss/rss.xml"
-   "http://www.dumbingofage.com/feed/"
-   "http://www.giantitp.com/comics/oots.rss"
-   "http://www.girlgeniusonline.com/ggmain.rss"
-   "http://www.goodmath.org/blog/?feed=rss2"
-   "http://www.kiwiblitz.com/rss.php"
-   "http://www.questionablecontent.net/QCRSS.xml"
-   "http://www.scottaaronson.com/blog/?feed=rss2"
-   "http://www.smbc-comics.com/rss.php"
-   "http://www.somethingpositive.net/sp.xml" 
-   "http://www.useswordonmonster.com/?feed=rss2"
-   "http://www.weeklystorybook.com/comic_strip_of_the_daycom/atom.xml"
-   "http://www.zachtronicsindustries.com/feed/"
-   "http://xkcd.com/atom.xml"
-   "http://youdamnkid.com/?feed=rss2"
-   "https://aphyr.com/posts.atom"
-   "https://yoo2080.wordpress.com/feed/"
-   "http://frivolesque.com/feed"))
+   ("http://mathbabe.org/feed/" blog)
+   ("http://thealexandrian.net/feed" rpg blog)
+   ("http://what-if.xkcd.com/feed.atom" comics blog)
+   ("http://www.weeklystorybook.com/comic_strip_of_the_daycom/atom.xml"
+    comics blog)
+   ("http://www.zachtronicsindustries.com/feed/" games blog)))
 
 (provide 'init-elfeed)
