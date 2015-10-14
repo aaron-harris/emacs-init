4bfd391ded56e0f52902957d17c517e87c778d3e
Aaron Harris <meerwolf@gmail.com>
2015-10-14 17:01:37 -0500

    Add and configure Helm
    We are now using Helm for completion, replacing Ido.  This commit
    makes the replacement, configures some Helm settings, and updates many
    keybindings.  Most of the changed keybindings are Helm-related, but a
    few are not.  In particular, we begin using the `C-z` prefix to launch
    processes which might be considered "subapps" of Emacs (e.g., `eww`
    and `elfeed`).

---
+++ init-helm.el
@@ -0,0 +1,40 @@
+;;; -*- lexical-binding: t -*-
+
+;;;; The Emacs init file of Aaron Harris.
+;;;; HELM CONFIGURATION
+;;;;============================================================================
+
+(require 'helm)
+(require 'helm-config)
+
+
+;;; Basic Settings
+;;;===============
+(helm-mode 1)
+(helm-autoresize-mode t)
+(setq helm-split-window-in-side-p t)           ; Leave window splits alone
+(setq helm-ff-search-library-in-sexp t)        ; Like `ffap' for `require'
+(setq helm-scroll-amount 8)                    ; For `C-M-v', `C-S-M-v'
+(setq helm-ff-file-name-history-use-recentf t) ; Better file history
+
+
+;;; Info Sources
+;;;=============
+;; Info pages to use for `helm-info-at-point'.
+(setq helm-info-default-sources
+      '(helm-source-info-emacs
+        helm-source-info-elisp
+        helm-source-info-cl
+        helm-source-info-eieio
+        helm-source-info-org
+        helm-source-info-pages))
+
+
+;;; Handler Setup
+;;;==============
+;; Turn Helm off for Org-mode refiling, since Helm can't handle
+;; multiple levels of refile targets.
+(add-to-list 'helm-completing-read-handlers-alist
+             '(org-refile . nil))
+
+(provide 'init-helm)
