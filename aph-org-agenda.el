fb001d5e81842d63a25151f55e4fa187f994d26e
Aaron Harris <meerwolf@gmail.com>
2015-10-29 18:05:15 -0500

    Enable use of habit graphs in all agenda types
    Add capability for habit graphs to be displayed in any agenda type,
    subject to the value of the variable
    `aph/org-habit-show-graphs-everywhere`.  This is accomplished by
    advising `org-agenda-finalize`.

--- aph-org-agenda.el
+++ aph-org-agenda.el
@@ -39,7 +39,6 @@ that value."
 ;;; Skip Functions
 ;;;===============
 ;;; These are functions to be used with `org-agenda-skip-function'.
-
 (defun aph/org-agenda-skip-without-match (match)
   "Skip current headline if it matches MATCH.
 
@@ -248,5 +247,42 @@ See `aph/weighted-comparator-maker' for more information."
                  (string-to-number it)
                  (funcall aph/org-comparator-rating-to-weight it)))))
     (funcall (aph/weighted-comparator-maker bias-fn) x y)))
+
+
+;;; Habits
+;;;=======
+(defvar aph/org-habit-show-graphs-everywhere nil
+  "If non-nil, show habit graphs in all types of agenda buffers.
+
+Normally, habits display consistency graphs only in
+\"agenda\"-type agenda buffers, not in other types of agenda
+buffers.  Set this variable to any non-nil value to show
+consistency graphs in all Org mode agendas.")
+
+(defun aph/org-agenda-mark-habits ()
+  "Mark all habits in current agenda for graph display.
+
+This function enforces `aph/org-habit-show-graphs-everywhere' by
+marking all habits in the current agenda as such.  When run just
+before `org-agenda-finalize' (such as by advice; unfortunately,
+`org-agenda-finalize-hook' is run too late), this has the effect
+of displaying consistency graphs for these habits.
+
+When `aph/org-habit-show-graphs-everywhere' is nil, this function
+has no effect."
+  (when (and aph/org-habit-show-graphs-everywhere
+         (not (get-text-property (point) 'org-series)))
+    (let ((cursor (point))
+          item data) 
+      (while (setq cursor (next-single-property-change cursor 'org-marker))
+        (setq item (get-text-property cursor 'org-marker))
+        (when (and item (org-is-habit-p item)) 
+          (with-current-buffer (marker-buffer item)
+            (setq data (org-habit-parse-todo item))) 
+          (put-text-property cursor
+                             (next-single-property-change cursor 'org-marker)
+                             'org-habit-p data))))))
+
+(advice-add #'org-agenda-finalize :before #'aph/org-agenda-mark-habits)
 
 (provide 'aph-org-agenda)
