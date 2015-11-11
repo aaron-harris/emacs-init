57315c47de9a117208c8d3035a30aa73639b68b2
Aaron Harris <meerwolf@gmail.com>
2015-11-11 14:49:06 -0600

    Repackage `shr/urlify-advice`
    Move `aph/shr-urlify-advice` from init-web.el to the new file
    aph-shr.el, and rewrite the application of that advice as a
    `use-package` block.  This is part of the grand `use-package`
    refactoring currently in progress.

--- init-web.el
+++ init-web.el
@@ -10,23 +10,11 @@
 
 ;;; Link Displey
 ;;;=============
-;; Do not underline image links, as this causes problems when using
-;; `shr-zoom-image'.
-(defface aph/shr-link-img
-  '((t (:inherit shr-link :underline nil)))
-  "Font for image links."
-  :group 'shr)
-
-(defun aph/shr-urlify-advice (start url &optional title)
-  "Make `shr-urlify' use face `aph/shr-link-img' for image links.
-Intended as :before advice for `shr-urlify'." 
-  (when (get-text-property start 'image-url)
-    (aph/advice-once #'shr-add-font :filter-args
-                     (lambda (args)
-                       (setf (nth 2 args) 'aph/shr-link-img)
-                       args))))
-
-(advice-add #'shr-urlify :before #'aph/shr-urlify-advice)
+(use-package shr
+  :defer t
+  :config
+  (use-package aph-shr)
+  (advice-add #'shr-urlify :before #'aph/shr-urlify-advice))
 
 
 (provide 'init-web)
