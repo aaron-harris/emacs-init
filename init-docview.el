f17b5a5d4400b31a07aafb7bc8e75fa91ccfb12f
Aaron Harris <aaron.patrick.harris@gmail.com>
2015-11-13 18:28:54 -0600

    Collect `doc-view` config into a `use-package` block

--- init-docview.el
+++
@@ -1,20 +0,0 @@
-;;; -*- lexical-binding: t -*-
-
-;;;; The Emacs inits file of Aaron Harris.
-;;;; DOCVIEW CONFIGURATION
-;;;;============================================================================
-
-
-;;; Program Setup
-;;;==============
-;; On mpc, Ghostview has a different name.
-(when (eq aph/machine 'mpc)
-  (setq doc-view-ghostscript-program "mgs.exe"))
-
-
-;;; Miscellaneous Settings
-;;;=======================
-;; Increase resolution for PDF images.
-(setq doc-view-resolution 200) 
-
-(provide 'init-docview)
