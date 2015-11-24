d107b43b95ef40801b8d3b5b0bce5a4584c0024d
Aaron Harris <aaron.patrick.harris@gmail.com>
2015-11-24 12:22:37 -0600

    Refactor `aph/rectangle-repetition-fix` to use `use-package`
    Move the definition of `aph/rectangle-repetition-fix` to the new
    module aph-rect.el (and delete the now-empty file init-misc.el).  Put
    the code applying the fix into a `use-package` block in init-core.el,
    and add an explicit version check (although this isn't strictly
    necessary).

--- aph-rect.el
+++ aph-rect.el
@@ -1,14 +1,12 @@
 ;;; -*- lexical-binding: t -*-
 
 ;;;; The Emacs init files of Aaron Harris:
-;;;; MISCELLANEOUS SETTINGS
-;;;;============================================================================
-
-(require 'aph-require)                  ; For `aph/require-softly'
+;;;; RECTANGLE EXTENSIONS
+;;;;============================================================================ 
 
 
-;;; Rectangle Patch
-;;;================
+;;; Emacs 25 Rectangle Motion Patch
+;;;================================
 ;; There seems to be a bug in Emacs 25 where movement commands in
 ;; rectangle mode hang when given a prefix argument.  This is a
 ;; workaround until the issue is fixed.
@@ -27,8 +25,5 @@ used as advice :around `rectangle--*-char'."
    ((> n 0)  (dotimes (i (1- n) (funcall fn cmd 1 other-cmd))
                (funcall fn cmd 1 other-cmd)))))
 
-;; Since the function `rectangle--*-char' is not defined in Emacs 24,
-;; this next statement will fail silently if we don't need the fix.
-(advice-add #'rectangle--*-char :around #'aph/rectangle-repetition-fix)
 
-(provide 'init-misc)
+(provide 'aph-rect)
