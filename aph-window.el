98232f866dc2b7af5cad97408f866cea20716338
Aaron Harris <meerwolf@gmail.com>
2015-11-29 13:02:18 -0600

    Delete commands `aph/scroll-*-by-line`
    The commands `aph/scroll-up-by-line` and `aph/scroll-down-by-line`
    were reimplementations of the built-in commands `scroll-up-line` and
    `scroll-down-line`.  This commit removes their definitions and rebinds
    their keys to their built-in equivalents.

--- aph-window.el
+++ aph-window.el
@@ -176,25 +176,4 @@ t.  This allows the RIDE parameter to be used interactively."
   (interactive "p")
   (aph/swap-buffer-backward count :ride)) 
 
-
-;;; Per-Line Scrolling
-;;;===================
-(defun aph/scroll-down-by-line (&optional arg)
-  "As `scroll-down-command', but ARG defaults to 1.
-
-Also, a negative prefix argument is treated as -1, scrolling only
-one line upward."
-  ;; All of our changes are encapsulated in the `interactive' form.
-  (interactive "^p")
-  (scroll-down-command arg))
-
-(defun aph/scroll-up-by-line (&optional arg)
-  "As `scroll-up-command', but ARG defaults to 1.
-
-Also, a negative prefix argument is treated as -1, scrolling only
-one line downward."
-  ;; All of our changes are encapsulated in the `interactive' form.
-  (interactive "^p")
-  (scroll-up-command arg)) 
-
 (provide 'aph-window)
