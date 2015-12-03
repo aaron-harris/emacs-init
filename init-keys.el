60019dfcc1c4323eb4b9c91e1ec372ee40e15856
Aaron Harris <meerwolf@gmail.com>
2015-12-03 17:17:30 -0600

    Move `eww` keybindings into `use-package` declarations
    Move the following keybindings from init-keys.el into `use-package`
    declarations in init-core.el (unless otherwise specified, into the
    declaration for the `eww` package):
    
    Global bindings:
    - `C-z C-w` for `eww`
    
    In `eww-mode-map`:
    - `S-<tab>` for `shr-previous-link`
    - `[` for `eww-previous-url`
    - `]` for `eww-next-url`
    - `z` for `shr-zoom-image`
    - `p` for `aph/elfeed-show-prev` (package `aph-elfeed`)
    - `n` for `aph/elfeed-show-next` (package `aph-elfeed`)
    
    Remove the code that unbound `p` and `n` in `eww-mode-map` when
    `elfeed` is not yet loaded, because we want the `use-package`
    declarations to be order-agnostic.  (We could accomplish this easily
    by moving the `p` and `n` bindings into the `eww` declaration and
    using a `with-eval-after-load` form, but this seems wrong, since the
    bindings are specifically related to `elfeed` control.)

--- init-keys.el
+++ init-keys.el
@@ -10,26 +10,6 @@
 
 ;;; Other Keybindings
 ;;;==================
-;; Eww
-(aph/global-set-keys-safely
-    ((kbd "C-z C-w")  #'eww))
-(with-eval-after-load 'eww
-  (aph/define-keys-safely eww-mode-map
-    ;; Clear keys for later rebinding in presence of `elfeed'
-    ((kbd "p")        nil :rebind) ; `eww-previous-url', see below
-    ((kbd "n")        nil :rebind) ; `eww-next-url', see below
-    ;; Intrapage navigation
-    ((kbd "S-<tab>")  #'shr-previous-link)
-    ;; Interpage navigation
-    ((kbd "[")        #'eww-previous-url) ; Moved from p
-    ((kbd "]")        #'eww-next-url)     ; Moved from n
-    ;; Image commands
-    ((kbd "z")        #'shr-zoom-image))
-  (with-eval-after-load 'elfeed
-    (aph/define-keys-safely eww-mode-map
-      ((kbd "p")      #'aph/elfeed-show-prev)
-      ((kbd "n")      #'aph/elfeed-show-next))))
-
 ;; Info
 (aph/global-set-keys-safely
   ((kbd "C-h i")  #'aph/info-mode-or-clone-buffer :rebind)) ; `info'
