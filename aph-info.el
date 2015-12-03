252fd55b7b1435c13f47df6f5fa0c893cce9b4f5
Aaron Harris <meerwolf@gmail.com>
2015-12-03 17:30:18 -0600

    Move `info` keybindings into `use-package` declaration
    Move the following keybindings from init-keys.el into the
    `use-package` declaration for `aph-info` in init-core.el:
    - `C-h i` for `aph/info-mode-or-clone-buffer` globally
    - `0` for `aph/Info-final-menu-item` in `Info-mode-map`
    
    Also remove the autoloads for these two commands (as these are
    unnecessary with `use-package`), and add the missing `provide`
    statement to aph-info.el.

--- aph-info.el
+++ aph-info.el
@@ -6,7 +6,6 @@
 
 ;; This file contains commands for use in `Info-mode'.
 
-;;;###autoload
 (defun aph/info-mode-or-clone-buffer (prefix)
   "Enter info mode or clone info buffer.
 
@@ -19,7 +18,6 @@ mode (as `info')."
     (setq prefix-arg prefix)
     (call-interactively #'info)))
 
-;;;###autoload
 (defun aph/Info-final-menu-item ()
   "Go to the node of the last menu item.
 
@@ -27,3 +25,5 @@ This command duplicates the functionality of the 0 key in the
 standalone info application."
   (interactive)
   (Info-goto-node (Info-extract-menu-counting nil)))
+
+(provide 'aph-info)
