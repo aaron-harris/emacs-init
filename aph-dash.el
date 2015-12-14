6bcb1c3bd03bcf7e514462447621b95455021412
Aaron Harris <meerwolf@gmail.com>
2015-12-14 17:16:16 -0600

    Move `dash` extension functions to aph-dash.el
    Move the functions `aph/reduce`, `aph/reductions`, and
    `aph/successor-in-list` from aph-lib.el to aph-dash.el.  Update all
    files using these functions as appropriate.
    
    Also, add a `use-package` declaration for `dash`.  Since `dash`
    doesn't need any configuration and defines no commands, this is mostly
    for `:ensure`.
    
    Note that `aph-dash` requires `dash`, but loading `dash` does not
    automatically load `aph-dash`.  This seems appropriate in this case,
    but is unusual for the `aph-*` modules.

---
+++ aph-dash.el
@@ -0,0 +1,46 @@
+;;; -*- lexical-binding: t -*-
+
+;;;; The Emacs init file of Aaron Harris.
+;;;; DASH EXTENSIONS
+;;;;============================================================================
+
+;;; Extensions to the `dash' library.
+
+(require 'dash)
+
+
+;;; Reduce
+;;;=======
+(defun aph/reduce (fn seq)
+  "As `-reduce', but accept all sequences, not just lists."
+  (if (listp seq)
+      (-reduce fn seq)
+    (-reduce fn (append seq nil))))
+
+(defun aph/reductions (fn seq)
+  "As `aph/reduce', but return intermediate results.
+These results are returned as a list."
+  (->> seq
+       (aph/reduce (lambda (acc val) 
+                     (let ((acc (if (listp acc) acc (list acc))))
+                       (cons (funcall fn (car acc) val) acc))))
+       (reverse)))
+
+
+;;; Element Search
+;;;===============
+(defun aph/successor-in-list (list elt &optional cycle)
+  "Return the element in LIST following ELT.
+If ELT is not an element of LIST, return nil.
+
+If ELT is the last element in LIST, return (car LIST) if the
+optional parameter CYCLE is non-nil; otherwise, return nil."
+  (let ((found  (-drop-while (lambda (x) (not (equal x elt)))
+                             list)))
+    (cond
+     ((null found)                   nil)
+     ((or (cdr found) (null cycle))  (cadr found))
+     (:else                          (car list)))))
+
+
+(provide 'aph-dash)
