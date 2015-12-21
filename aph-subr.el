7d59841c1542fa94f83700b20900903348ef3fb1
Aaron Harris <aaron.patrick.harris@gmail.com>
2015-12-21 13:01:51 -0600

    Move `aph/get-bol` and `aph/get-eol` to `aph-subr`
    Move the functions `aph/get-bol` and `aph/get-eol` from `init-draft`
    to `aph-subr`.  This is preparatory to moving `aph/number-lines`,
    etc. out of `init-draft`.
    
    Also move the test associated with these functions to the new test
    module `aph-subr-test`.

--- aph-subr.el
+++ aph-subr.el
@@ -7,6 +7,29 @@
 ;; Extensions for `subr' module.
 
 
+;;; Buffer Position Functions
+;;;==========================
+(defun aph/get-bol (&optional pos)
+  "Return position of first character on line containing POS.
+If POS is omitted, use position of point.
+
+Do not move point."
+  (save-excursion
+    (let ((pos (or pos (point))))
+      (goto-char pos)
+      (line-beginning-position))))
+
+(defun aph/get-eol (&optional pos)
+  "Return position of last character on line containing POS.
+If POS is omitted, use position of point.
+
+Do not move point."
+  (save-excursion
+    (let ((pos (or pos (point))))
+      (goto-char pos)
+      (line-end-position))))
+
+
 ;;; Alist Functions
 ;;;================
 (defun aph/assoc-delete-all (key alist)
