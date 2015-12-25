ac4523b7e0a5380a130a29417beb65afd3009cd9
Aaron Harris <aaron.patrick.harris@gmail.com>
2015-12-25 14:09:11 -0600

    Implement targeted message suppression in `aph-silence` module
    To use, modify the variables `aph/silence-enabled` and `aph/silence-list`.

---
+++ aph-silence.el
@@ -0,0 +1,80 @@
+;;; -*- lexical-binding: t -*-
+
+;;;; The Emacs init files of Aaron Harris:
+;;;; SILENCE
+;;;;============================================================================
+
+;; This module implements targeted message suppression.
+
+
+;;; Configuration Variables
+;;;========================
+(defvar aph/silence-list nil
+  "A list identifying messages to be silenced.
+
+Valid entries are strings and functions.
+
+A string entry is interpreted as a regexp; messages containing a match
+will be silenced.  Note that the entire message doesn't have to
+match.
+
+A function entry is called for each message, with the same args
+as the triggering `message' call, and the message is silenced if
+the function returns non-nil.
+
+See `aph/silence-advice' for more details.")
+
+(defvar aph/silence-enabled nil
+  "If non-nil, some messages will be silenced.
+
+To control which messages are silenced, see `aph/silence-list'.
+See `aph/silence-advice' for more details.")
+
+
+;;; Message Suppression
+;;;====================
+(defun aph/silence-message-p (msg)
+  "Return non-nil if MSG should be silenced per `aph/silence-list'.
+If `aph/silence-enabled' is nil, return nil for any MSG.  See
+`aph/silence-advice' for more details.
+
+MSG should be a string, already formatted, just as it would
+appear in the *Messages* buffer."
+  (require 'dash)                       ; For `-some-p'
+  (when aph/silence-enabled
+    (-some-p
+     (lambda (elt)
+       (cond
+        ((stringp elt)    (string-match-p elt msg))
+        ((functionp elt)  (funcall elt msg))
+        :else             (error "Invalid entry %s in `aph/silence-list'" elt)))
+     aph/silence-list)))
+
+(defun aph/silence-advice (fun &rest args)
+  "Advice to enforce `aph/silence-enabled'.
+
+If `aph/silence-enabled' is nil, do nothing.
+
+Otherwise, consult `aph/silence-list' to see whether the message
+defined by ARGS should be silenced (where ARGS is the arglist for
+the pending `message' command.
+
+If a message is silenced, `format' is called instead of
+`message'.  Thus, the return value is unchanged, but no message
+is shown in the *Messages* buffer.
+
+This function is intended as :around advice for `message'.  As
+`message' is a primitive, this is not ideal, but there doesn't
+seem to be any way to achieve this kind of message suppression
+otherwise.  Because `message' is a primitive, not all messages
+can be silenced; calls from C code may avoid being silenced." 
+  (let ((msg (apply #'format args)))
+    (if (aph/silence-message-p msg)
+        msg
+      (apply fun args))))
+
+;; Note that, with the default settings, this advice does nothing.
+(advice-add #'message :around #'aph/silence-advice)
+
+
+(provide 'aph-silence)
