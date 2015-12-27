4ea0f409c5246d2abd1a261f3c518b51bfff0c53
Aaron Harris <meerwolf@gmail.com>
2015-12-27 09:26:54 -0600

    Revert load message suppression
    This implementation had significant bugs, and this approach was not
    well-considered.  Better to just get rid of it.

--- test/aph-silence-test.el
+++ test/aph-silence-test.el
@@ -71,23 +71,5 @@
         (should (equal (message "%s" "") ""))
         (should (equal (current-message) "bar"))))))
 
-
-;;; Load Message Suppression
-;;;=========================
-(ert-deftest aph/silence-test-load-messages ()
-  "Test `aph/silence-load-messages'."
-  ;; Mock `load' for testing:
-  (cl-letf (((symbol-function 'load) 
-             (lambda (file &optional noerror nomessage nosuffix must-suffix)
-               nomessage)))
-    ;; Check that above replacement function is working.
-    (should-not (load "foo"))
-    (should (load "foo" nil t))
-    (aph/silence-load-messages
-      ;; Check that loading messages are suppressed.
-      (should (load "foo")))
-    ;; Check cleanup
-    (should-not (load "foo"))))
-
       
 (provide 'aph-advice-test)
