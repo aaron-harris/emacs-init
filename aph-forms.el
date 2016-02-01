d162dc00f36220598fc426ecc012d9ddaa05bc75
Aaron Harris <meerwolf@gmail.com>
2016-02-01 14:46:49 -0600

    New command `aph/helm-forms-list-records`
    This command is designed to be invoked from within a `forms-mode`
    database and allows the user to jump to any record in that database.
    
    The name displayed for each record in the helm buffer is the value of
    field number 1 for that record.  At present there is no easy way to
    change this; future commits will improve the functionality.

--- aph-forms.el
+++ aph-forms.el
@@ -56,6 +56,24 @@ initial record."
       (run-hooks 'aph/forms-change-record-hook))))
 
 
+;;; Database Query Functions
+;;;=========================
+(defun aph/forms-list-records (&optional name-field)
+  "Return an alist mapping NAME-FIELD to record number for current db.
+
+For use in `forms-mode'.  The returned alist has one entry per
+record in the current database, with each entry of the form
+\(NAME . NUM), where NAME is the value of the field whose number
+is specified by NAME-FIELD and NUM is the number of the record.
+
+If omitted, NAME-FIELD defaults to 1."
+  (let ((name-field  (or name-field 1)))
+    (aph/forms-reduce
+     (lambda (acc)
+       (cons `(,(nth name-field forms-fields) . ,forms--current-record)
+             acc)))))
+
+
 ;;; Auxiliary File
 ;;;===============
 (defvar-local aph/forms-aux-buffer nil
