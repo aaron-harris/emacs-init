31f38344a6a35bb3737747cca8dc14615ca5caf7
Aaron Harris <meerwolf@gmail.com>
2016-02-04 18:08:52 -0600

    Rename `aph/with-test-mode` to `aph/ert-with-test-mode`
    Also move this macro from `aph-mode-tag-test` to `aph-ert-test`.

--- test/aph-ert-test.el
+++ test/aph-ert-test.el
@@ -16,5 +16,48 @@
   (should (aph/ert-macro-executes-body let (canary)))
   (should-error (aph/ert-macro-executes-body ignore)))
 
+
+;;; Mode Testing Apparatus Tests
+;;;=============================
+(ert-deftest aph/ert-test-with-test-mode--body ()
+  "Test that body of `aph/ert-with-test-mode' is executed."
+  (aph/ert-macro-executes-body aph/ert-with-test-mode (mode) 'text-mode))
+
+(ert-deftest aph/ert-test-with-test-mode--bindings ()
+  "Test that `aph/ert-with-test-mode' sets bindings correctly."
+  ;; Intentional bindings
+  (aph/ert-with-test-mode (mode hook) 'text-mode
+    (should (fboundp mode))
+    (should (eq hook (aph/symbol-concat mode "-hook")))
+    (should (boundp hook)))
+  ;; No unintentional bindings
+  (aph/ert-with-test-mode (mode) 'text-mode
+    (should-error hook      :type 'void-variable)
+    (should-error make-mode :type 'void-variable)))
+
+(ert-deftest aph/ert-test-with-test-mode--nesting ()
+  "Test that `aph/ert-with-test-mode' nests properly."
+  (aph/ert-with-test-mode (mode1) 'text-mode
+    (aph/ert-with-test-mode (mode2) mode1
+      (should (eq 'text-mode (get mode1 'derived-mode-parent)))
+      (should (eq mode1      (get mode2 'derived-mode-parent))))))
+
+(ert-deftest aph/ert-test-with-test-mode--cleanup ()
+  "Test that `aph/ert-with-test-mode' cleans up after itself."
+  ;; Normal exit
+  (let (mode-x hook-x)
+    (aph/ert-with-test-mode (mode hook) 'text-mode
+      (setq mode-x mode
+            hook-x hook)))
+  ;; Error
+  (let (mode-x hook-x)
+    (ignore-errors
+      (aph/ert-with-test-mode (mode hook) 'text-mode
+        (setq mode-x mode
+              hook-x hook)
+        (error "Triggered error")))
+    (should-not (intern-soft mode-x))
+    (should-not (intern-soft hook-x)))) 
+
 
 (provide 'aph-ert-test)
