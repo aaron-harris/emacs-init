94cfbb34038b4a28731083c317d23dcb77582501
Aaron Harris <meerwolf@gmail.com>
2016-02-09 13:08:27 -0600

    New function `aph/update-alist` and macro `aph/set-assoc`
    The function `aph/update-alist` allows for in-place updating of alist
    associations, and the macro `aph/set-assoc` goes further by also
    handling back-assignment to variables.

--- test/aph-subr-test.el
+++ test/aph-subr-test.el
@@ -7,6 +7,9 @@
 ;; Tests for the module aph-subr.el.
 (require 'aph-subr)
 
+
+;;; Buffer Position Functions
+;;;==========================
 (ert-deftest aph/test-get-bol/-eol ()
   "Test functions `aph/get-bol' and `aph/get-eol'."
   (with-temp-buffer
@@ -30,6 +33,29 @@
     (should (= (aph/get-bol 8) (aph/get-eol 8) 8))
     (should (= (aph/get-bol 9) (aph/get-bol) 9))
     (should (= (aph/get-eol 9) (aph/get-eol) 12 (point)))))
+
+
+;;; Alist Functions
+;;;================
+(ert-deftest aph/test-update-alist ()
+  "Test `aph/update-alist'."
+  (let ((alist '((foo . 1) (bar . 2) (foo . 3))))
+    (should (equal (setq alist (aph/update-alist alist 'foo 4))
+                   '((foo . 4) (bar . 2) (foo . 3))))
+    (should (equal (setq alist (aph/update-alist alist 'bar 5))
+                   '((foo . 4) (bar . 5) (foo . 3))))
+    (should (equal (setq alist (aph/update-alist alist 'baz 6))
+                   '((baz . 6) (foo . 4) (bar . 5) (foo . 3))))))
+
+(ert-deftest aph/set-assoc ()
+  "Test `aph/set-assoc'."
+  (let ((alist '((foo . 1) (bar . 2) (foo . 3))))
+    (should (equal (aph/set-assoc alist 'foo 4)
+                   '((foo . 4) (bar . 2) (foo . 3))))
+    (should (equal (aph/set-assoc alist 'bar 5)
+                   '((foo . 4) (bar . 5) (foo . 3))))
+    (should (equal (aph/set-assoc alist 'baz 6)
+                   '((baz . 6) (foo . 4) (bar . 5) (foo . 3))))))
 
       
 (provide 'aph-subr-test)
