5e559b51ab6e3b163681ddf2468554ff58ab9ecc
Aaron Harris <aaron.patrick.harris@gmail.com>
2016-03-02 15:29:09 -0600

    Split `aph/outline-before-first-heading-p`
    Rename the function `aph/outline-before-first-heading-p` to be
    `aph/outline-before-first-heading`, and add a new function
    `aph/outline-before-first-heading-p` that has the same return value
    but does not change the match data; this is analogous to the
    difference between `looking-at` and `looking-at-p`.
    
    The reason for this is my newfound understanding that the
    function-variable `outline-level` needs the match data to be set
    before it can be used, and that most of the `outline-mode` functions
    set this data.  I would prefer to isolate this state as much as
    possible in my own code, so I need to make the distinction as
    carefully as possible.

--- aph-outline.el
+++ aph-outline.el
@@ -10,14 +10,21 @@
 
 ;;; Information Functions
 ;;;======================
-(defun aph/outline-before-first-heading-p (&optional invisible-ok)
+(defun aph/outline-before-first-heading (&optional invisible-ok)
   "Return non-nil if before first visible heading of buffer.
-If INVISIBLE-OK is non-nil, also consider invisible headings."
+If INVISIBLE-OK is non-nil, also consider invisible headings.
+
+Set match data for `outline-level'."
+  ;; The match data is set implicitly by `outline-back-to-heading'.
   (condition-case err
       (save-excursion
         (outline-back-to-heading invisible-ok)
         nil)
     (error t)))
+
+(defun aph/outline-before-first-heading-p (&optional invisible-ok)
+  "As `aph/outline-before-first-heading', but save match data."
+  (save-match-data (aph/outline-before-first-heading invisible-ok)))
 
 
 (provide 'aph-outline)
