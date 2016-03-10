b294706125b76e1be5362f98e8424f44ee5d3462
Aaron Harris <aaron.patrick.harris@gmail.com>
2016-03-10 12:29:29 -0600

    Invert `aph/outline--*-heading`
    The DIR parameter for this function was not well-named.  A better name
    for the parameter is REVERSE, as this makes it more apparent which
    direction non-nil corresponds to.  As a consequence of this change,
    the semantics of that parameter have been reversed.

--- aph-outline.el
+++ aph-outline.el
@@ -45,18 +45,18 @@ In any case, do not assume that match data reflects
 
 ;;; Navigation Functions
 ;;;=====================
-(defun aph/outline--*-heading (arg dir &optional invisible-ok)
+(defun aph/outline--*-heading (arg reverse &optional invisible-ok)
   "Used by `aph/outline-next-heading', `aph/outline-previous-heading'.
-If DIR is non-nil, behave as `aph/outline-next-heading';
-otherwise, behave as `aph/outline-previous-heading'."
-  (let ((univ-func  (if dir #'outline-next-heading
-                      #'outline-previous-heading))
-        (vis-func   (if dir #'outline-next-visible-heading
-                      #'outline-previous-visible-heading))
+If REVERSE is non-nil, behave as `aph/outline-previous-heading';
+otherwise, behave as `aph/outline-next-heading'."
+  (let ((univ-func  (if reverse #'outline-previous-heading
+                      #'outline-next-heading))
+        (vis-func   (if reverse #'outline-previous-visible-heading
+                      #'outline-next-visible-heading))
         (start      (point)))
     (cond
      ((< arg 0)           (aph/outline--*-heading
-                           (- arg) (not dir) invisible-ok))
+                           (- arg) (not reverse) invisible-ok))
      ((not invisible-ok)  (funcall vis-func arg))
      (:else               (dotimes (i arg (point))
                             (funcall univ-func))))
@@ -68,7 +68,7 @@ If INVISIBLE-OK is non-nil, call `outline-next-heading' ARG
 times (or `outline-previous-heading' -ARG times if ARG is
 negative).  Otherwise, defer to `outline-next-visible-heading'."
   (interactive "p")
-  (aph/outline--*-heading arg t invisible-ok))
+  (aph/outline--*-heading arg nil invisible-ok))
 
 (defun aph/outline-previous-heading (arg &optional invisible-ok)
   "As `outline-previous-visible-heading', but maybe invisible too.
@@ -76,7 +76,7 @@ If INVISIBLE-OK is non-nil, call `outline-previous-heading' ARG
 times (or `outline-previous-heading' -ARG times if ARG is
 negative).  Otherwise, defer to `outline-previous-visible-heading'."
   (interactive "p")
-  (aph/outline--*-heading arg nil invisible-ok))
+  (aph/outline--*-heading arg t invisible-ok))
 
 (defun aph/outline-top-heading (&optional invisible-ok)
   "Move up to top-level visible heading above current, and return point.
