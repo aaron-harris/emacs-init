83b76c8b892d16540ecdee5a37a3221646513bb5
Aaron Harris <meerwolf@gmail.com>
2016-03-15 17:51:18 -0500

    New command `aph/sp-kill-sentence`
    This command, bound to `M-k` in `smartparens` mode, acts like
    `kill-sentence` but ensures that we don't kill past a string or
    comment boundary.

---
+++ aph-smartparens.el
@@ -0,0 +1,36 @@
+;;; -*- lexical-binding: t -*-
+
+;;;; The Emacs init files of Aaron Harris:
+;;;; SMARTPARENS EXTENSIONS
+;;;;============================================================================
+
+;; Extensions for `smartparens' package.
+(require 'smartparens)
+
+
+;;; String Protection
+;;;==================
+(defun aph/sp-kill-sentence (&optional arg)
+  "As `kill-sentence', but don't kill past end of current context.
+
+In a string or comment, kill either to the end of sentence (as
+`kill-sentence') or to the end of the string or comment,
+whichever is nearer.  Do not kill a closing string or comment
+delimiter.  Treat ARG in the same way as `kill-sentence'.
+
+Outside of strings and comments, this should generally behave as
+`kill-sentence', but no guarantees are made."
+  (interactive "p")
+  (let ((context          (sp--get-context))
+        (end-of-sentence  (save-excursion (forward-sentence arg))))
+    (kill-region (point)
+                 (progn
+                   (while (and (< (point) end-of-sentence)
+                               (eq (sp--get-context) context))
+                     (forward-char))
+                   (unless (eq (sp--get-context) context)
+                     (backward-char))
+                   (point)))))
+
+
+(provide 'aph-smartparens)
---
+++ test/aph-smartparens-test.el
@@ -0,0 +1,51 @@
+;;; -*- lexical-binding: t -*-
+
+;;;; The Emacs init file of Aaron Harris.
+;;;; SMARTPARENS TESTS
+;;;;============================================================================
+
+;; Tests for the module aph-smartparens.el.
+(require 'aph-smartparens)
+(require 'aph-ert)
+
+
+;;; String Protection
+;;;==================
+(defun aph/sp-test-kill-sentence-1 (lines words chars start desired)
+  "Subroutine for `aph/sp-test-kill-sentence'.
+
+Move forward by LINES, WORDS, and CHARS; check that we're
+`looking-at-p' START (a string); call `aph/sp-kill-sentence'; and
+finally check that we're `looking-at-p' DESIRED."
+  (declare (indent 3))
+  (unless (zerop lines) (forward-line lines))
+  (forward-word words)
+  (forward-char chars)
+  (should (looking-at-p start))
+  (aph/sp-kill-sentence)
+  (should (looking-at-p desired)))
+
+(ert-deftest aph/sp-test-kill-sentence ()
+  "Test `aph/sp-kill-sentence'."
+  (aph/ert-with-buffer 'emacs-lisp-mode "
+\(defun foo (arglist)
+  \"This is a docstring.\"
+  (do-stuff-here)
+  ;; This is a comment
+  (and \"foo\" \"bar\"))"
+    (aph/sp-test-kill-sentence-1 1 1 0
+      " is a docstring."
+      "\"")
+    (aph/sp-test-kill-sentence-1 2 1 0
+      " is a comment" 
+      "\n  (and \"foo\"")
+    (aph/sp-test-kill-sentence-1 1 1 2
+      "foo\" \"bar\")" 
+      "\" \"bar\")"))
+  (aph/ert-with-buffer 'text-mode "
+Sentence one.  Sentence two."
+    (aph/sp-kill-sentence)
+    (should (looking-at-p "  Sentence two."))))
+
+
+(provide 'aph-smartparens-test)
