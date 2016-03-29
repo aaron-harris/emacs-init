56f8423537800b2d45a377c9e3e3218dbd6ebf3c
Aaron Harris <aaron.patrick.harris@gmail.com>
2016-03-29 12:20:12 -0500

    Standardize heading for `umbra-test` module

--- test/umbra-test.el
+++ test/umbra-test.el
@@ -1,10 +1,28 @@
-;;; -*- lexical-binding: t -*-
+;;; umbra-test.el --- Tests for umbra.el             -*- lexical-binding: t; -*-
 
-;;;; The Emacs init file of Aaron Harris.
-;;;; UMBRA TESTS
-;;;;============================================================================
+;; Copyright (C) 2016  Aaron Harris
 
-;; Tests for `umbra-mode'.
+;; Author: Aaron Harris <meerwolf@gmail.com>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
 (require 'umbra)
 (require 'aph-ert-test)
 
@@ -268,3 +286,4 @@ and 'penumbra' is either the keyword :penumbra or nil."
 
 
 (provide 'umbra-test)
+;;; umbra-test.el ends here
