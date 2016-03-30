66074f165dd254d2ab75d8f89b9f0cfa9afa5f44
Aaron Harris <meerwolf@gmail.com>
2016-03-30 11:57:51 -0500

    Remove `aph/offline-p`
    The function `aph/offline-p` wasn't doing very much, since at present
    I have only one machine that shouldn't be installing packages (`mpc`).
    It will be easy enough to reimplement the function if I need it.

--- init-core.el
+++ init-core.el
@@ -32,21 +32,16 @@
 
 (require 'package)
 
-(defun aph/offline-p ()
-  "Return non-nil if this machine should not install packages.
-The return value depends only on `aph/machine'."
-  (eq aph/machine 'mpc))
-
 (setq package-user-dir "~/sync/emacs/elpa") 
 (setq package-archives
-      (and (not (aph/offline-p))
+      (and (not (eq aph/machine 'mpc))
            '(("elpa"      . "http://elpa.gnu.org/packages/")
              ("marmalade" . "https://marmalade-repo.org/packages/")
              ("melpa"     . "https://melpa.org/packages/"))))
 (package-initialize)
 
 ;; Bootstrap `use-package'
-(unless (or (package-installed-p 'use-package) (aph/offline-p))
+(unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 (require 'use-package)
