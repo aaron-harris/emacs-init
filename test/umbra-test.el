5f158ede891c4500d0b57964498de026505c761f
Aaron Harris <aaron.patrick.harris@gmail.com>
2016-03-31 14:01:46 -0500

    Clean up header documentation in `umbra-test`

--- test/umbra-test.el
+++ test/umbra-test.el
@@ -4,6 +4,8 @@
 
 ;; Author: Aaron Harris <meerwolf@gmail.com>
 
+;; Required features: `umbra', `dash', `aph-dash', `aph-ert-test'
+
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
@@ -16,10 +18,6 @@
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;;; Commentary:
-
-;; 
 
 ;;; Code:
 
