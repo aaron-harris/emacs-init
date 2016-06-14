d22e2e46c328aeecc8daf60b49ed535565aed74d
Aaron Harris <aaron.patrick.harris@gmail.com>
2016-06-14 18:05:26 -0500

    Rewrite random comparators
    The random comparators previously written did not implement a true
    random shuffle.  This made non-uniform random comparators even less
    random.  The new implementation does implement a random shuffle, and
    is significantly better in a number of ways.  (In particular, the
    decision of whether the ordering should persist through an agenda
    refresh is made per-agenda.)
    
    Also remove the now-obsolete module `aph-comparators`.

--- org-compare.el
+++ org-compare.el
@@ -38,6 +38,19 @@
 ;;     based on their values for a specific property.  Options are
 ;;     provided for both string and numeric comparisons, as well as
 ;;     custom user-supplied transforms.
+;;
+;; `org-compare-randomly'
+;;
+;;     This meta-comparator implements a random shuffle.  Care is
+;;     taken to make sure the shuffle is fully random, and options are
+;;     provided both to make refreshing the agenda preserve the order,
+;;     and to make a refresh trigger a reshuffle.
+;;
+;; `org-compare-randomly-by'
+;;
+;;     As `org-compare-randomly', except the user can supply a
+;;     function to generate random keys, allowing for a non-uniform
+;;     distribution.
 
 ;;; Code:
 
@@ -45,15 +58,6 @@
 
 (eval-when-compile (require 'dash))
 (require 'seq)
-
-
-;;;; User Options
-;;===============
-(defgroup org-compare nil
-  "Comparators for the Org agenda."
-  :prefix "org-compare-"
-  :link   '(emacs-commentary-link "org-compare")
-  :group  'org-agenda)
 
 
 ;;;; Subroutines
@@ -76,6 +80,16 @@ compares strings and not agenda entries."
      ((> compare 0)  +1)
      ((< compare 0)  -1))))
 
+(defun org-compare--get-marker (entry)
+  "Return the marker for ENTRY.
+
+This marker points to the location of the headline referenced by
+ENTRY."
+  (get-text-property 1 'org-marker entry))
+
+
+;;;; Property Comparators
+;;=======================
 (defun org-compare--get-property (entry prop &optional numeric default)
   "Return the value of PROP in ENTRY.
 
@@ -88,17 +102,12 @@ a number.
 Normally, if ENTRY does not have a value for PROP, return nil.
 If the optional parameter DEFAULT is supplied, instead return
 that value."
-  (let ((raw-val
-         (-> (get-text-property 1 'org-marker entry)
-             (org-entry-get prop))))
+  (let ((raw-val (org-entry-get (org-compare--get-marker) prop)))
     (cond
      ((null raw-val)  (or default nil))
      (numeric         (string-to-number raw-val))
      (t                raw-val))))
 
-
-;;;; Comparators
-;;==============
 (defun org-compare-property (prop type &optional default transform)
   "Return a comparator that sorts by PROP.
 
@@ -149,6 +158,83 @@ does not, an error will be signaled."
          ((= x-val y-val)  nil)
          ((< x-val y-val)  -1)
          ((> x-val y-val)  +1))))))
+
+
+;;;; Random Comparators
+;;=====================
+(defvar org-compare-random-refresh nil
+  "Whether `org-compare-randomly' should refresh its keys.
+
+See the docs for `org-compare-randomly' for more information.")
+
+(defun org-compare-randomly--update-sort-key (entry table generator)
+  "Return sort key for ENTRY in TABLE, generating it if necessary.
+For internal use by `org-compare-randomly-by'."
+  (let* ((marker    (org-compare--get-marker entry))
+         (hash-key  `(,(marker-buffer marker) . ,(marker-position marker))))
+    (or (gethash hash-key table)
+        (puthash hash-key (funcall generator entry) table))))
+
+(defun org-compare-randomly-by (generator)
+  "Return a random comparator using GENERATOR.
+
+The comparator returned is like `org-compare-randomly', except
+the distribution of random keys is controlled by GENERATOR and
+may thus be non-uniform.
+
+The function GENERATOR is called with a single argument, an
+agenda entry, when that entry lacks a sort key.  It should return
+a number, which is then used for all comparisons until the key
+list is cleared; see `org-compare-randomly' for more details on
+this.
+
+Subsequent calls to `org-compare-randomly-by' produce comparators
+with independent sets of sort keys."
+  (let ((table (make-hash-table :test #'equal)))
+    (lambda (x y) 
+      (when org-compare-random-refresh 
+        (clrhash table)
+        (setq org-compare-random-refresh nil))
+      (let ((x-val (org-compare-randomly--update-sort-key x table generator))
+            (y-val (org-compare-randomly--update-sort-key y table generator)))
+        (cond
+         ((= x-val y-val)  nil)
+         ((< x-val y-val)   -1)
+         ((> x-val y-val)   +1))))))
+
+(defun org-compare-randomly ()
+  "Return a comparator implementing a random shuffle.
+
+When given distinct agenda entries X and Y, the resulting
+comparator has an equal chance of returning +1 and -1 (and a
+miniscule chance of returning nil).  Subsequent calls will result
+in results consistent with a total ordering.
+
+To accomplish this, a hash table of randomly-generated sort keys
+is maintained.  This table will persist until the comparator is
+called when the variable `org-compare-random-refresh' is non-nil.
+This means that setting this variable as part of a custom agenda
+command using this comparator as `org-agenda-cmp-user-defined'
+will cause the sort order to change whenever the agenda is
+refreshed; otherwise, it will persist until Emacs is restarted.
+
+Note that if you don't want the sort order to change on refresh,
+you need to be careful that the comparator is created when the
+custom agenda command is defined, not when it's called, e.g.
+
+    (add-to-list
+     'org-agenda-custom-commands
+     `(\"y\" \"Example Agenda\"
+       ((todo
+         \"\"
+         ((org-agenda-cmp-user-defined ',(org-compare-randomly))
+          (org-agenda-sorting-strategy '(user-defined-up)))))))
+
+\(Notice the use of backquote.)
+
+Comparators resulting from different calls to this function have
+independent key tables."
+  (org-compare-randomly-by (lambda (_) (random))))
 
 (provide 'org-compare)
 ;;; org-compare.el ends here
