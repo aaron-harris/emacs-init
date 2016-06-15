66619f66e18dafbc018af4c3c4414db7a7c27a5d
Aaron Harris <meerwolf@gmail.com>
2016-06-15 13:20:54 -0500

    Move "habits everywhere" code to new module `org-habit-everywhere`
    Also introduce a barb module for `org`, named `org-barb`, to collect
    as new hooks all advice injection into `org-mode`.  I should probably
    go through my other `org`-modifying modules and see if anything else
    should be turned into a hook.

--- aph-org-agenda.el
+++ aph-org-agenda.el
@@ -20,41 +20,4 @@ This is exactly the command bound by default to g in
   (interactive)
   (org-agenda-redo t))
 
-
-;;; Habits
-;;;=======
-(defvar aph/org-habit-show-graphs-everywhere nil
-  "If non-nil, show habit graphs in all types of agenda buffers.
-
-Normally, habits display consistency graphs only in
-\"agenda\"-type agenda buffers, not in other types of agenda
-buffers.  Set this variable to any non-nil value to show
-consistency graphs in all Org mode agendas.")
-
-(defun aph/org-agenda-mark-habits ()
-  "Mark all habits in current agenda for graph display.
-
-This function enforces `aph/org-habit-show-graphs-everywhere' by
-marking all habits in the current agenda as such.  When run just
-before `org-agenda-finalize' (such as by advice; unfortunately,
-`org-agenda-finalize-hook' is run too late), this has the effect
-of displaying consistency graphs for these habits.
-
-When `aph/org-habit-show-graphs-everywhere' is nil, this function
-has no effect."
-  (when (and aph/org-habit-show-graphs-everywhere
-         (not (get-text-property (point) 'org-series)))
-    (let ((cursor (point))
-          item data) 
-      (while (setq cursor (next-single-property-change cursor 'org-marker))
-        (setq item (get-text-property cursor 'org-marker))
-        (when (and item (org-is-habit-p item)) 
-          (with-current-buffer (marker-buffer item)
-            (setq data (org-habit-parse-todo item))) 
-          (put-text-property cursor
-                             (next-single-property-change cursor 'org-marker)
-                             'org-habit-p data))))))
-
-(advice-add #'org-agenda-finalize :before #'aph/org-agenda-mark-habits)
-
 (provide 'aph-org-agenda)
