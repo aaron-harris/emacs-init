10893b79ccf480cde628011ed948f7cdac74c9e5
Aaron Harris <aaron.patrick.harris@gmail.com>
2016-06-16 11:58:05 -0500

    Update header style for `aph-page` module

--- aph-page.el
+++ aph-page.el
@@ -1,10 +1,31 @@
-;;; -*- lexical-binding: t -*-
+;;; aph-page.el --- Extensions for `page' module     -*- lexical-binding: t; -*-
 
-;;;; The Emacs init files of Aaron Harris:
-;;;; PAGE EXTENSIONS
-;;;;============================================================================
+;; Copyright (C) 2016  Aaron Harris
 
-;; Extensions for `page' package.
+;; Author: Aaron Harris <meerwolf@gmail.com>
+;; Keywords: convenience
+
+;; Dependencies: `page', `hydra'
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Extensions for the `page' module built into Emacs.
+
+;;; Code:
+
 (require 'page)
 (require 'hydra)
 
@@ -17,5 +38,5 @@
   ("["        backward-page "back")
   ("<return>" nil           "quit" :color blue))
 
-
 (provide 'aph-page)
+;;; aph-page.el ends here
