a76f25e4f384a3d605fcc346cddb06178e3ed074
Aaron Harris <meerwolf@gmail.com>
2016-06-16 17:01:23 -0500

    Clean up `aph-simple` module and related code
    In addition to the usual process of cleaning up package headers,
    delete `aph/yank-command-on-string`, as this function is not currently
    being used, has rather questionable utility, and would be easy to
    reimplement in any case.

--- test/aph-simple-test.el
+++ test/aph-simple-test.el
@@ -1,31 +1,32 @@
-;;; -*- lexical-binding: t -*-
+;;; aph-simple-test.el --- Tests for aph-simple.el   -*- lexical-binding: t; -*-
 
-;;;; The Emacs init file of Aaron Harris.
-;;;; SIMPLE TESTS
-;;;;============================================================================
+;; Copyright (C) 2016  Aaron Harris
 
-;; Tests for the module `aph-simple'.
+;; Author: Aaron Harris <meerwolf@gmail.com>
+
+;; Dependencies: `aph-simple', `ert'
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (require 'aph-simple)
+(require 'ert)
 
 
-;;; Yank Commands
-;;;==============
-(ert-deftest aph/test-yank-command-on-string ()
-  "Test `aph/yank-command-on-string'." 
-  (let (kill-ring)
-    (with-temp-buffer
-      (should (equal "foo"
-                     (aph/yank-command-on-string "foo" #'yank)))
-      (should (equal "" (buffer-string)))
-      (should (equal "foo"
-                     (aph/yank-command-on-string "bar" #'yank 2)))
-      (should (equal "" (buffer-string)))
-      (should (equal "baz"
-                     (aph/yank-command-on-string "bar" #'insert "baz"))))))
-
-
-;;; Motion Commands
-;;;================
+;;;; Motion Commands
+;;==================
 (ert-deftest aph/simple-test-move-bol ()
   "Test `aph/move-beginning-of-line'."
   (with-temp-buffer
@@ -37,6 +38,6 @@
     (open-line 1)                                         ; "  |"/"foo"
     (should (= 4 (aph/move-beginning-of-line 2) (point))) ; "  "/"|foo"
     ))
-
       
 (provide 'aph-simple-test)
+;;; aph-simple-test.el ends here
