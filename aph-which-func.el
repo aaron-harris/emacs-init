94bdff007ca3c2d5ba7e72802182d2a86379e591
Aaron Harris <meerwolf@gmail.com>
2016-06-17 19:01:40 -0500

    New module `which-func-header`
    This module collects code formerly found in `aph-which-func` moving
    `which-function-mode` info into the header line, and reformats it as a
    minor mode, `which-func-header-mode`.

--- aph-which-func.el
+++ aph-which-func.el
@@ -5,34 +5,6 @@
 ;;;;============================================================================
 
 ;; Functions extending module `which-func'
-(require 'which-func)
-
-(require 'aph-subr)                     ; For `aph/assoc-delete-in'
-
-
-;;; Header Line Display
-;;;====================
-;; This code is adapted from a snippet I found here:
-;;   https://www.emacswiki.org/emacs/WhichFuncMode
-
-(defvar aph/which-func-header-line-format
-  '(which-func-mode ("" which-func-format))
-  "Header line format construct for `which-function-mode'.
-Not enabled until `aph/which-func-use-header-line' has been
-called.")
-
-(defun aph/which-func-use-header-line-advice ()
-  "Advice making `which-function-mode' use the header line.
-Applied by `aph/which-func-use-header-line'.
-Intended as :after advice for `which-func-ff-hook'."
-  (when which-func-mode 
-    (aph/assoc-delete-in mode-line-misc-info 'which-func-mode)
-    (setq header-line-format aph/which-func-header-line-format)))
-
-(defun aph/which-func-use-header-line ()
-  "Move `which-function-mode' info to the header line." 
-  (aph/which-func-use-header-line-advice)
-  (advice-add 'which-func-ff-hook :after #'aph/which-func-use-header-line-advice))
 
 
 ;;; Org Mode Support
---
+++ which-func-header.el
@@ -0,0 +1,107 @@
+;;; which-func-header.el --- Show `which-func` info in header -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  Aaron Harris
+
+;; Author: Aaron Harris <meerwolf@gmail.com>
+;; Keywords: tools, mode-line, imenu
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This module defines the minor mode `which-func-header-mode'.  This
+;; mode is an extension to `which-function-mode' that places the
+;; information in the header line, rather than the mode line.
+;;
+;; Unlike `which-function-mode', `which-func-header-mode' is
+;; buffer-local.  A globalized mode is planned, but has not yet been
+;; implemented.  Therefore, to use `which-func-header-mode', you
+;; should put `which-func-header-mode' in the mode hooks for whatever
+;; major modes strike you as appropriate.
+
+;;; Code:
+
+(require 'which-func)
+(require 'aph-subr)
+
+(defcustom which-func-header-format
+  '(which-function-mode (which-func-header-mode ("" which-func-format)))
+  "Header line format construct for `which-func-header-mode'.
+
+When `which-func-header-mode' is on, this will be used as
+`header-line-format' (overriding any previous value)."
+  :type 'sexp)
+
+;;;###autoload
+(define-minor-mode which-func-header-mode
+  "Minor mode moving `which-function-mode' info to header.
+
+This minor mode causes information normally shown by
+`which-function-mode' in the mode line to instead be shown in the
+header line.  If `which-function-mode' is disabled, nothing is
+shown in either the header line or the mode line.
+
+Unlike `which-function-mode', this mode is buffer-local."
+  :group       'which-func
+  :init-value  nil
+  :require     'which-func-header 
+  (if (and which-function-mode which-func-header-mode) 
+      (let ((construct (assq 'which-func-mode mode-line-misc-info)))
+        (when construct
+          (aph/assq-delete-in mode-line-misc-info 'which-func-mode)
+          (aph/assq-delete-in mode-line-misc-info 'which-func-header-mode)
+          (push `(which-func-header-mode "" ,construct) mode-line-misc-info))
+        (setq header-line-format which-func-header-format))
+    (kill-local-variable 'header-line-format))
+  (add-hook 'which-function-mode-hook
+            #'which-func-header-mode:refresh nil :local))
+
+(defun which-func-header-mode:on ()
+  "Turn on `which-func-header-mode'."
+  (which-func-header-mode 1))
+
+(defun which-func-header-mode:off ()
+  "Turn off `which-func-header-mode'."
+  (which-func-header-mode -1))
+
+(defun which-func-header-mode:refresh ()
+  "Refresh header line display for `which-func-header-mode'.
+
+It is possible to toggle `which-function-mode' independently of
+`which-func-header-mode'.  Doing so can cause the contents of the
+header line to become out of sync with the state of
+`which-func-header-mode'.  To prevent this,
+`which-func-header-mode' puts this function (which just
+re-asserts the current value of `which-func-header-mode' in
+`which-functino-mode-hook'.)"
+  (which-func-header-mode (if which-func-header-mode 1 -1)))
+
+(defun which-func-header-unload-function ()
+  "Undo changes made to Emacs for `which-func-header-mode'."
+  ;; Turn off `which-func-header-mode' in all buffers.
+  ;; This should restore changes to `header-line-format'.
+  (mapc (lambda (buf)
+          (with-current-buffer buf
+            (when which-func-header-mode (which-func-header-mode -1))))
+        (buffer-list))
+  ;; Restore changes to `mode-line-misc-info'.
+  (let ((construct (nth 2 (assq 'which-func-header-mode mode-line-misc-info))))
+    (when construct
+      (aph/assq-delete-in mode-line-misc-info 'which-func-header-mode)
+      (push construct mode-line-misc-info)))
+  ;; Let normal unloading proceed
+  nil)
+
+(provide 'which-func-header)
+;;; which-func-header.el ends here
