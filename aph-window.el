ab10ad234f2bbea6571ca91cc94e55670ad855de
Aaron Harris <aaron.patrick.harris@gmail.com>
2016-06-21 11:59:51 -0500

    Add package header for `aph-window` module

--- aph-window.el
+++ aph-window.el
@@ -1,10 +1,32 @@
-;;; -*- lexical-binding: t -*-
+;;; aph-window.el --- Extensions for `window' module -*- lexical-binding: t; -*-
 
-;;;; The Emacs init files of Aaron Harris:
-;;;; WINDOW EXTENSIONS
-;;;;============================================================================
+;; Copyright (C) 2016  Aaron Harris
 
-;; Extensions to the `window' module.
+;; Author: Aaron Harris <meerwolf@gmail.com>
+;; Keywords: convenience
+
+;; Dependencies: `window'
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Extensions for the `window' module built into Emacs.
+
+;;; Code:
+
+(require 'window)
 (require 'hydra)
 
 
@@ -24,5 +46,5 @@
   ("M-v" (scroll-other-window '-) "back") 
   ("C-g" nil                      "quit" :color blue))
 
-
 (provide 'aph-window)
+;; aph-window.el ends here
