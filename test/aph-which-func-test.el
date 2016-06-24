445afbfe3e573ff352676c3a4f7af2ae7f9070ec
Aaron Harris <aaron.patrick.harris@gmail.com>
2016-06-24 16:41:35 -0500

    Clean up header for `aph-which-func-test`

--- test/aph-which-func-test.el
+++ test/aph-which-func-test.el
@@ -1,10 +1,26 @@
-;;; -*- lexical-binding: t -*-
+;;; aph-which-func-test.el --- Tests for aph-which-func.el  -*- lexical-binding: t; -*-
 
-;;;; The Emacs init file of Aaron Harris.
-;;;; WHICH-FUNCTION-MODE TESTS
-;;;;============================================================================
+;; Copyright (C) 2016  Aaron Harris
 
-;; Tests for the module `aph-which-func'.
+;; Author: Aaron Harris <meerwolf@gmail.com>
+
+;; Depenencies: `aph-which-func', `proctor'
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (require 'aph-which-func)
 (require 'proctor)
 
@@ -33,5 +49,5 @@ Text under Heading 1"
     (should (aph/which-function-test-org-1 "Text under Heading 1"
                                            "Heading 1"))))
 
-
 (provide 'aph-which-func-test)
+;;; aph-which-func-test.el ends here
