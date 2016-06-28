08212a5894ac4805058df94563801ad4aa83b86f
Aaron Harris <aaron.patrick.harris@gmail.com>
2016-06-28 17:38:14 -0500

    New function `eimp-ephemeral-apply`
    This function is the core of the `eimp-ephemeral` module.  Any EIMP
    transform invoked with this function will not mark the image buffer as
    modified.

--- init-draft.el
+++ init-draft.el
@@ -10,33 +10,5 @@
 
 ;;; Code:
 
-
-;;;; EIMP
-;;=======
-(defun aph/eimp-fit-image-to-window (&optional arg)
-    "As `eimp-fit-image-to-window', but do not mark buffer as modified.
-Also, ARG is optional, defaulting to nil."
-    (let ((buffer     (current-buffer))
-          (advice-id  (cl-gensym "aph/eimp-fit-image-to-window:")))
-      ;; This is clearly a hack, and I should probably find a better
-      ;; way to do this.
-      (advice-add #'eimp-mogrify-process-sentinel :after
-                  (lambda (proc msg)
-                    (cond
-                     ((not (buffer-live-p buffer))
-                      (advice-remove #'eimp-mogrify-process-sentinel advice-id))
-                     ((eq (process-buffer proc) buffer)
-                      (with-current-buffer buffer
-                        (set-buffer-modified-p nil)))))
-                  `((name . ,advice-id)))
-      ;; This wrapper fixes the situation where the current buffer is
-      ;; not in the selected window, e.g., if we just opened the file
-      ;; using `find-file-other-window'.
-      (if (equal (selected-window) (get-buffer-window buffer))
-          (eimp-fit-image-to-window arg) 
-        (with-selected-window (next-window) ; Super-kludgy!
-          (with-current-buffer buffer
-            (eimp-fit-image-to-window arg))))))
-
 (provide 'init-draft)
 ;; init-draft.el ends here
