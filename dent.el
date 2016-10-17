00f917a138bf1fa29fee9d18ee4a81514d2df170
Aaron Harris <meerwolf@gmail.com>
2016-10-17 14:07:45 -0500

    Add `declare` support for `dent-indent-specform`
    It is now possible to declare that a function or macro should use
    `dent-indent-specform`, together with the desired value of
    `dent-indent-function-spec`, directly in the function or macro's
    `declare` spec, using the `dent-indent` property (which functions like
    `indent` for numeric arguments, except it uses `dent-indent-specform`
    rather than `lisp-indent-specform`).

--- dent.el
+++ dent.el
@@ -5,6 +5,8 @@
 ;; Author: Aaron Harris <meerwolf@gmail.com>
 ;; Keywords: lisp
 
+;; Dependencies: `map'
+
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
@@ -24,6 +26,8 @@
 
 ;;; Code:
 
+(require 'map)
+
 (defun dent-indent-specform (indent-point state)
   "Indentation function for macros with several distinguished args.
 
@@ -33,15 +37,14 @@ indentation, not just the first two.
 
 The value for N is taken from the `dent-indent-function-spec'
 property associated with the function symbol for the sexp being
-indented.  E.g., to use this function and indent the first three
-arguments, you would write:
+indented.  This is set automatically by the `declare' property
+`dent-indent'; e.g., to use this function and indent the first
+three arguments, you would write:
 
     (defmacro foo (&rest args)
       \"Foo!\"
-      (declare (indent dent-indent-specform))
-      (...))
-
-    (put 'foo 'dent-indent-function-spec 3)"
+      (declare (dent-indent 3))
+      ...)"
   ;; This code is mostly copied directly from `lisp-indent-specform'.
   (let* ((normal-indent (current-column))
          (containing-form-start (elt state 1))
@@ -74,6 +77,48 @@ arguments, you would write:
               containing-form-start)
       ;; A non-distinguished form.  Use body-indent.
       body-indent)))
+
+
+;;;; Register `declare' property
+;;==============================
+(defun dent--set-indent (name _arglist indent)
+  "Set indent spec for function or macro NAME per INDENT.
+
+The function or macro will use `dent-indent-specform' to
+calculate its indentation, with the property
+`dent-indent-function-spec' set to INDENT.
+
+Intended for use in `defun-declarations-alist' and
+`macro-declarations-alist', enabling use of the `dent-indent'
+property in a `declare' form.
+
+For example, the function definition
+
+    (defun foo (&rest args)
+      \"Foo!\"
+      (declare (dent-indent 3))
+      ...)
+
+is equivalent to
+
+    (defun foo (&rest args)
+      \"Foo!\"
+      (declare (indent dent-indent-specform))
+      ...)
+    (put 'foo 'dent-indent-function-spec 3)"
+  (function-put name 'lisp-indent-function 'dent-indent-specform)
+  (function-put name 'dent-indent-function-spec indent))
+
+(dolist (alist '(defun-declarations-alist macro-declarations-alist))
+  (map-put (symbol-value alist) 'dent-indent (list #'dent--set-indent)))
+
+
+;;;; Unloading
+;;============
+(defun dent-unload-function ()
+  "Unregister `dent-indent' property for `declare'."
+  (dolist (alist '(defun-declarations-alist macro-declarations-alist))
+    (map-delete (symbol-value alist) 'dent-indent)))
 
 (provide 'dent)
 ;;; dent.el ends here
